0ce38ea077a5e3ee0181875d25eaba76c4c4b8ce
rungene <rungenesamuel@gmail.com>
2021-12-17 04:55:28 +0300

    refactor:removing companion object

--- app/src/main/java/com/example/plainolnotes4/MainFragment.kt
+++ app/src/main/java/com/example/plainolnotes4/MainFragment.kt
@@ -9,9 +9,6 @@ import android.view.ViewGroup
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
 
     private lateinit var viewModel: MainViewModel
 
