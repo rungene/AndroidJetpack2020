e2ff2412d8a82f2162e1afd26dd2d55826d6248a
rungene <rungenesamuel@gmail.com>
2021-12-17 04:54:39 +0300

    reafctor:removing onActivityCreated()

--- app/src/main/java/com/example/plainolnotes4/MainFragment.kt
+++ app/src/main/java/com/example/plainolnotes4/MainFragment.kt
@@ -23,10 +23,4 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
-
 }
