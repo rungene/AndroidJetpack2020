d026dccb09e3c8e84baf7bf5df02340d9e3f7412
rungene <rungenesamuel@gmail.com>
2021-12-20 05:26:57 +0300

    refactor:creating a visual divider  between each of the rows

--- app/src/main/java/com/example/plainolnotes4/MainFragment.kt
+++ app/src/main/java/com/example/plainolnotes4/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.plainolnotes4.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
@@ -20,7 +22,18 @@ class MainFragment : Fragment() {
     ): View? {
         binding = MainFragmentBinding.inflate(inflater,container,false)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        with(binding.recyclerView){
+            setHasFixedSize(true)
+            val divider =DividerItemDecoration(
+                context,LinearLayoutManager(context).orientation
+            )
+            addItemDecoration(divider)
+        }
+
+
         return binding.root
+
     }
 
 }
