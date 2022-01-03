583d4b66ec77549f303dda998bc3a760d1bff3d3
rungene <rungenesamuel@gmail.com>
2022-01-03 05:04:43 +0300

    refactor:Adding an init block

--- app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
+++ app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
@@ -3,9 +3,14 @@ package com.example.plainolnotes4
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.data.NoteEntity
+import com.example.data.SampleDataClass
 
 class MainViewModel : ViewModel() {
 
     val noteList = MutableLiveData<List<NoteEntity>>()
+
+    init {
+        noteList.value=SampleDataClass.getNotes()
+    }
 
 }
