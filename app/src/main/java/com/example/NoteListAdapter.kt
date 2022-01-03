669f45ba47ba4592840dd64beb2d6023b2ee139e
rungene <rungenesamuel@gmail.com>
2022-01-03 05:35:08 +0300

    refactor:creating ViewHolder class, initializing NoteListAdapter

--- app/src/main/java/com/example/NoteListAdapter.kt
+++ app/src/main/java/com/example/NoteListAdapter.kt
@@ -1,4 +1,22 @@
 package com.example
 
-class NoteListAdapter {
+import android.view.View
+import android.widget.AbsListView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.data.NoteEntity
+import com.example.plainolnotes4.databinding.ListItemBinding
+
+class NoteListAdapter (private val noteList: List<NoteEntity>) :
+RecyclerView.Adapter<NoteListAdapter.ViewHolder>()
+{
+
+
+    inner class ViewHolder(itemView: View) :
+    RecyclerView.ViewHolder(itemView)
+    {
+val binding =ListItemBinding.bind(itemView)
+    }
+
+
 }
