764859954ce1688608a30fdf9dd7d0586e14ff40
rungene <rungenesamuel@gmail.com>
2022-01-03 04:55:51 +0300

    refactor: Adding getNotes()

--- app/src/main/java/com/example/data/SampleDataClass.kt
+++ app/src/main/java/com/example/data/SampleDataClass.kt
@@ -17,5 +17,11 @@ class SampleDataClass {
         private fun getDate(diff:Long):Date{
             return Date(Date().time+diff)
         }
+
+        fun getNotes() = arrayListOf(
+            NoteEntity(1, getDate(0), sampleText1),
+            NoteEntity(2, getDate(1), sampleText2),
+            NoteEntity(3, getDate(2), sampleText3)
+        )
     }
 }
