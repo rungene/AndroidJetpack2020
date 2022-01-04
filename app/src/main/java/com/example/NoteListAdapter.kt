62d04ce846a1e1b8ba9634f1cbbbc7334e23f169
rungene <rungenesamuel@gmail.com>
2022-01-04 04:49:51 +0300

    refactor:Overriding onCreateViewHolder(), onBindViewHolder(), getItemCount()

--- app/src/main/java/com/example/NoteListAdapter.kt
+++ app/src/main/java/com/example/NoteListAdapter.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AbsListView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
@@ -11,11 +12,22 @@ class NoteListAdapter (private val noteList: List<NoteEntity>) :
 RecyclerView.Adapter<NoteListAdapter.ViewHolder>()
 {
 
-
     inner class ViewHolder(itemView: View) :
     RecyclerView.ViewHolder(itemView)
     {
 val binding =ListItemBinding.bind(itemView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 
 
