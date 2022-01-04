86c15ab53c7edb2dfe3019faf347c324a12a3b21
rungene <rungenesamuel@gmail.com>
2022-01-04 05:13:00 +0300

    refactor: Implementing onCreateViewHolder()

--- app/src/main/java/com/example/NoteListAdapter.kt
+++ app/src/main/java/com/example/NoteListAdapter.kt
@@ -1,11 +1,13 @@
 package com.example
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AbsListView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.data.NoteEntity
+import com.example.plainolnotes4.R
 import com.example.plainolnotes4.databinding.ListItemBinding
 
 class NoteListAdapter (private val noteList: List<NoteEntity>) :
@@ -17,9 +19,13 @@ RecyclerView.Adapter<NoteListAdapter.ViewHolder>()
     {
 val binding =ListItemBinding.bind(itemView)
     }
-
+//called each time new list is generated, gets those references to views & then returns viewholder
+    //object
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+       val inflater =LayoutInflater.from(parent.context)
+      val view = inflater.inflate(R.layout.list_item,parent,false)
+
+    return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
