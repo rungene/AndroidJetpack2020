7c55a901fb85adefb02c9f38c1d85068ba9d656b
rungene <rungenesamuel@gmail.com>
2022-01-09 09:18:43 +0300

    refactor:adding getAll()

--- app/src/main/java/com/example/data/NoteDAO.kt
+++ app/src/main/java/com/example/data/NoteDAO.kt
@@ -1,8 +1,10 @@
 package com.example.data
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface NoteDAO {
@@ -14,6 +16,11 @@ interface NoteDAO {
     //inserting a bunch of notes, if they already exist in db throw away the new ones
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertAll(notes: List<NoteEntity>)
+
+    //returns a LiveData object,allowing to observe the db table, use reactive programming using an
+    //an observer
+    @Query("SELECT * FROM notes ORDER BY date ASC")
+    fun getAll() : LiveData<List<NoteEntity>>
 
 
 }
