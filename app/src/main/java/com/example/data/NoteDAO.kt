7904e36ffb3efd0e9f08da0cb748f1836b1fbf11
rungene <rungenesamuel@gmail.com>
2022-01-09 09:03:11 +0300

    refactor: adding insertNote()

--- app/src/main/java/com/example/data/NoteDAO.kt
+++ app/src/main/java/com/example/data/NoteDAO.kt
@@ -1,4 +1,16 @@
 package com.example.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
 interface NoteDAO {
+
+    //this function can be used to insert a new note or update the existing note
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(noteEntity: NoteEntity)
+
+
+
 }
