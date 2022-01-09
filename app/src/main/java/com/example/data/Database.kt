5ffcdcfd91d1527b7f63d16b0210a26d4ced41b1
rungene <rungenesamuel@gmail.com>
2022-01-09 15:32:28 +0300

    refactor:Adding Database annotation

--- app/src/main/java/com/example/data/Database.kt
+++ app/src/main/java/com/example/data/Database.kt
@@ -1,4 +1,9 @@
 package com.example.data
 
-class Database {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
+abstract class Database :RoomDatabase() {
+
 }
