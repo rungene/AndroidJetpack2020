e6f445123a0a208616d7561757c4c92a12ab4875
rungene <rungenesamuel@gmail.com>
2022-01-09 16:32:58 +0300

    refactor:Implementing getInstance()

--- app/src/main/java/com/example/data/DatabaseApp.kt
+++ app/src/main/java/com/example/data/DatabaseApp.kt
@@ -2,6 +2,7 @@ package com.example.data
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import java.util.*
@@ -19,7 +20,16 @@ abstract class DatabaseApp :RoomDatabase() {
         private var INSTANCE : DatabaseApp?=null
 
         fun getInstance(context:Context):DatabaseApp?{
-
+            if (INSTANCE== null){
+   //ensures initialization of DB is only executed once at a time
+                synchronized(DatabaseApp::class){
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        DatabaseApp::class.java,
+                        "notes.db"
+                    ).build()
+                }
+            }
 
             return INSTANCE
         }
