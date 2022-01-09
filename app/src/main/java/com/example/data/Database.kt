367708ee6d6c4197485ac7241f1444bdf5ad32dd
rungene <rungenesamuel@gmail.com>
2022-01-09 16:04:02 +0300

    refactor:registering TypeConverter

--- app/src/main/java/com/example/data/Database.kt
+++ app/src/main/java/com/example/data/Database.kt
@@ -2,8 +2,11 @@ package com.example.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import java.util.*
 
 @Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class Database :RoomDatabase() {
 
 }
