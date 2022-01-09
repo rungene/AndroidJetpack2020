fc878226c89df471d0a8f73371470f07f2ce7788
rungene <rungenesamuel@gmail.com>
2022-01-09 16:21:09 +0300

    refactor:Initialize DB

--- app/src/main/java/com/example/data/DatabaseApp.kt
+++ app/src/main/java/com/example/data/DatabaseApp.kt
@@ -1,5 +1,6 @@
 package com.example.data
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
@@ -7,14 +8,22 @@ import java.util.*
 
 @Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
-abstract class Database :RoomDatabase() {
+abstract class DatabaseApp :RoomDatabase() {
 
     //register DAO
     abstract fun noteDao() :NoteDAO?
 
     //initialize DB
     companion object{
-        
+
+        private var INSTANCE : DatabaseApp?=null
+
+        fun getInstance(context:Context):DatabaseApp?{
+
+
+            return INSTANCE
+        }
+
     }
 
 }
