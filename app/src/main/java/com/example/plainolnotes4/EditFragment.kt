b6e49197f08eac13f98339af1b6fb357b872b7e4
rungene <rungenesamuel@gmail.com>
2022-01-09 07:36:32 +0300

    refactor: display the icon as an up button

--- app/src/main/java/com/example/plainolnotes4/EditFragment.kt
+++ app/src/main/java/com/example/plainolnotes4/EditFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.navArgs
 import com.example.plainolnotes4.databinding.EditFragmentBinding
 
@@ -19,6 +20,16 @@ class EditFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+    // getting a reference to the activity that owns this fragment,get a reference to supportActionBar
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_check)
+        }
+
+
         binding= EditFragmentBinding.inflate(inflater,container,false)
         binding.editText.setText("you selected note number ${args.noteId}")
 
