773c6ab5509aea6004e6bfad8378c0f595fce275
rungene <rungenesamuel@gmail.com>
2022-01-09 15:07:58 +0300

    refactor:adding dateToTimeStamp()

--- app/src/main/java/com/example/data/Converters.kt
+++ app/src/main/java/com/example/data/Converters.kt
@@ -4,10 +4,15 @@ import androidx.room.TypeConverter
 import java.util.*
 
 class Converters {
-    //used when you want to take an integer value from db and pass it back to Kotlin code as a date object
+    //used when you want to take an integer value from db and pass it back to Kotlin code as a
+    // date object
     @TypeConverter
     fun fromTimeStamp(value:Long):Date{
         return Date(value)
     }
 
+    @TypeConverter
+    fun dateToTimeStamp(date: Date):Long{
+        return date.time
+    }
 }
