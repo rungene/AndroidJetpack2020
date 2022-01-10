f082f61ba6e7ff31c89668578266cfce70dbb555
rungene <rungenesamuel@gmail.com>
2022-01-10 04:37:16 +0300

    refactor:Adding setup code

--- app/src/androidTest/java/com/example/plainolnotes4/DatabaseTest.kt
+++ app/src/androidTest/java/com/example/plainolnotes4/DatabaseTest.kt
@@ -1,4 +1,21 @@
 package com.example.plainolnotes4
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
 class DatabaseTest {
+
+
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.example.plainolnotes4", appContext.packageName)
+    }
 }
