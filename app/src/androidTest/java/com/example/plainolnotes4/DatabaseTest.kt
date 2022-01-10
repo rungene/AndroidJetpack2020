ae8f0595f54128d33e8ddbebdf87949bc701b05c
rungene <rungenesamuel@gmail.com>
2022-01-10 04:43:43 +0300

    refactor: Adding properties

--- app/src/androidTest/java/com/example/plainolnotes4/DatabaseTest.kt
+++ app/src/androidTest/java/com/example/plainolnotes4/DatabaseTest.kt
@@ -2,6 +2,8 @@ package com.example.plainolnotes4
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.example.data.DatabaseApp
+import com.example.data.NoteDAO
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,7 +12,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class DatabaseTest {
 
-
+    private lateinit var dao: NoteDAO
+    private lateinit var databaseApp: DatabaseApp
 
     @Test
     fun useAppContext() {
