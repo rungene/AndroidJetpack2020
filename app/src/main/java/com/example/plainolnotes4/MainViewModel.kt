b7ea7855794523edc6258df919c796cf89cf1b45
rungene <rungenesamuel@gmail.com>
2022-01-12 17:48:34 +0300

    refactor:Creating database property and initializing it

--- app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
+++ app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
@@ -4,10 +4,13 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.data.DatabaseApp
 import com.example.data.NoteEntity
 import com.example.data.SampleDataClass
 
 class MainViewModel(app:Application) : AndroidViewModel(app) {
+
+    private val database =DatabaseApp.getInstance(app)
 
     val noteList = MutableLiveData<List<NoteEntity>>()
 
