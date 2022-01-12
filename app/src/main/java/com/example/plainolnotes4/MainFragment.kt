86b4b3c875061c90225c8ec2ab5e3445d8625a43
rungene <rungenesamuel@gmail.com>
2022-01-12 05:06:02 +0300

    refactor:Adding onCreateOptionsMenu()

--- app/src/main/java/com/example/plainolnotes4/MainFragment.kt
+++ app/src/main/java/com/example/plainolnotes4/MainFragment.kt
@@ -3,10 +3,8 @@ package com.example.plainolnotes4
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -48,6 +46,11 @@ class MainFragment : Fragment(), NoteListAdapter.ListItemListener{
         })
         return binding.root
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main,menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onItemClick(noteId: Int) {
