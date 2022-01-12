d93d1fd5887bddea3dea692ca1ec3bef760e8f25
rungene <rungenesamuel@gmail.com>
2022-01-12 18:08:30 +0300

    refactor:Adding addSampleData() function

--- app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
+++ app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
@@ -4,9 +4,13 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.data.DatabaseApp
 import com.example.data.NoteEntity
 import com.example.data.SampleDataClass
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel(app:Application) : AndroidViewModel(app) {
 
@@ -15,5 +19,14 @@ class MainViewModel(app:Application) : AndroidViewModel(app) {
     val noteList = database?.noteDao()?.getAll()
 
 
+     fun addSampleData(){
+         viewModelScope.launch {
+             withContext(Dispatchers.IO){
+                 val sampleNotes = SampleDataClass.getNotes()
+                 database?.noteDao()?.insertAll(sampleNotes)
+             }
+         }
+
+     }
 
 }
