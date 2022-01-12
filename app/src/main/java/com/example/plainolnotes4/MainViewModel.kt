a3fd533f6ca4a876ccbfb0713b9b216bb8c3ee96
rungene <rungenesamuel@gmail.com>
2022-01-12 17:36:55 +0300

    refactor:Changing the superclass to Android ViewModel, changing class constructor

--- app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
+++ app/src/main/java/com/example/plainolnotes4/MainViewModel.kt
@@ -1,11 +1,13 @@
 package com.example.plainolnotes4
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.data.NoteEntity
 import com.example.data.SampleDataClass
 
-class MainViewModel : ViewModel() {
+class MainViewModel(app:Application) : AndroidViewModel(app) {
 
     val noteList = MutableLiveData<List<NoteEntity>>()
 
