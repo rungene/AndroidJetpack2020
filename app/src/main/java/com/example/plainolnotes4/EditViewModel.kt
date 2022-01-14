43c777e52eb387bb339d834b1d845b89c27366d6
rungene <rungenesamuel@gmail.com>
2022-01-14 22:46:53 +0300

    refactor: Adding a currentNote val ,initializing it

--- app/src/main/java/com/example/plainolnotes4/EditViewModel.kt
+++ app/src/main/java/com/example/plainolnotes4/EditViewModel.kt
@@ -2,11 +2,15 @@ package com.example.plainolnotes4
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.data.DatabaseApp
+import com.example.data.NoteEntity
 
 class EditViewModel (app:Application): AndroidViewModel(app) {
 
     private val database = DatabaseApp.getInstance(app)
+    val currentNote = MutableLiveData<NoteEntity>()
+
     // TODO: Implement the ViewModel
 }
