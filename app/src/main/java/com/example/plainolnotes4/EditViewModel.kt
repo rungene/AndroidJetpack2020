b57ee9714837d8f67181cd9ed06d9f81535af59e
rungene <rungenesamuel@gmail.com>
2022-01-14 22:44:40 +0300

    refactor:Adding a private variable abd initializing it

--- app/src/main/java/com/example/plainolnotes4/EditViewModel.kt
+++ app/src/main/java/com/example/plainolnotes4/EditViewModel.kt
@@ -3,7 +3,10 @@ package com.example.plainolnotes4
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.example.data.DatabaseApp
 
 class EditViewModel (app:Application): AndroidViewModel(app) {
+
+    private val database = DatabaseApp.getInstance(app)
     // TODO: Implement the ViewModel
 }
