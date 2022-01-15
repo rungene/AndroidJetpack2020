572b461436074b90756afedf9b36f22c4178f5e3
rungene <rungenesamuel@gmail.com>
2022-01-15 15:12:39 +0300

    refactor:Adding constant

--- app/src/main/java/com/example/plainolnotes4/Constants.kt
+++ app/src/main/java/com/example/plainolnotes4/Constants.kt
@@ -4,3 +4,4 @@ const val NEW_NOTE_ID = 0
 const val TAG = "Log_Note"
 const val NOTES_TEXT_KEY ="noteTextKey"
 const val CURSOR_POSITION_KEY ="cursorPositionKey"
+const val SELECTED_NOTE_KEY = "selectedNotesKey"
